4ae1b3f87ce7eef17edcedf1541e62fb1c73ae77
IlyaZinkovich <ilya.zinkovich@gmail.com>
2015-02-07 20:36:20 +0300

    Spring Tutorial - 10 Autowiring

--- src/main/java/tutorial/Triangle.java
+++ src/main/java/tutorial/Triangle.java
@@ -4,20 +4,37 @@ import java.util.List;
 
 public class Triangle {
 
-    public List<Point> getPoints() {
-        return points;
+    private Point pointA;
+    private Point pointB;
+    private Point pointC;
+
+    public Point getPointA() {
+        return pointA;
     }
 
-    public void setPoints(List<Point> points) {
-        this.points = points;
+    public void setPointA(Point pointA) {
+        this.pointA = pointA;
     }
 
-    private List<Point> points;
+    public Point getPointB() {
+        return pointB;
+    }
 
+    public void setPointB(Point pointB) {
+        this.pointB = pointB;
+    }
 
+    public Point getPointC() {
+        return pointC;
+    }
 
+    public void setPointC(Point pointC) {
+        this.pointC = pointC;
+    }
 
     public void draw() {
-        points.forEach(System.out::println);
+        System.out.println(pointA);
+        System.out.println(pointB);
+        System.out.println(pointC);
     }
 }
