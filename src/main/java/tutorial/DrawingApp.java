cc274232f0e89a8e8ac9cef7aaaf938c51f3b4f3
IlyaZinkovich <ilya.zinkovich@gmail.com>
2015-02-07 22:02:46 +0300

    Spring Tutorial - 14 Callbacks

--- src/main/java/tutorial/DrawingApp.java
+++ src/main/java/tutorial/DrawingApp.java
@@ -1,11 +1,13 @@
 package tutorial;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class DrawingApp {
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("spring-config.xml");
+        AbstractApplicationContext context = new ClassPathXmlApplicationContext("spring-config.xml");
+        context.registerShutdownHook();
         Triangle triangle = (Triangle) context.getBean("triangle");
         triangle.draw();
     }
--- src/main/java/tutorial/Triangle.java
+++ src/main/java/tutorial/Triangle.java
@@ -2,12 +2,14 @@ package tutorial;
 
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 
 import java.util.List;
 
-public class Triangle implements ApplicationContextAware, BeanNameAware {
+public class Triangle implements ApplicationContextAware, BeanNameAware, InitializingBean, DisposableBean {
 
     private List<Point> points;
 
@@ -33,5 +35,23 @@ public class Triangle implements ApplicationContextAware, BeanNameAware {
 
     public void setPoints(List<Point> points) {
         this.points = points;
+    }
+
+    public void init() {
+        System.out.println("Initialization");
+    }
+
+    public void destroyBean() {
+        System.out.println("Destroy");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("Initializing Bean init method");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("Disposable Bean destroy method");
     }
 }
