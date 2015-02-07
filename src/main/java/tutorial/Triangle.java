e751e61d1d709ef380e3e5803acef4eac0450324
IlyaZinkovich <ilya.zinkovich@gmail.com>
2015-02-07 19:15:24 +0300

    Spring Tutorial - 05

--- src/main/java/tutorial/Triangle.java
+++ src/main/java/tutorial/Triangle.java
@@ -1,7 +1,18 @@
 package tutorial;
 
 public class Triangle {
+
+    private String type;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
     public void draw() {
-        System.out.println("Draw Triangle");
+        System.out.println(type + " Triangle");
     }
 }
