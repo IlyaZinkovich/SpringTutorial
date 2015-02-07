c112ebe3430e3186c5423b07b528413c1709d94c
IlyaZinkovich <ilya.zinkovich@gmail.com>
2015-02-07 19:15:24 +0300

    Spring Tutorial - 06

--- src/main/java/tutorial/Triangle.java
+++ src/main/java/tutorial/Triangle.java
@@ -3,6 +3,20 @@ package tutorial;
 public class Triangle {
 
     private String type;
+    private int height;
+
+    public Triangle(String type) {
+        this.type = type;
+    }
+
+    public Triangle(int height) {
+        this.height = height;
+    }
+
+    public Triangle(String type, int height) {
+        this.type = type;
+        this.height = height;
+    }
 
     public String getType() {
         return type;
@@ -12,7 +26,15 @@ public class Triangle {
         this.type = type;
     }
 
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
     public void draw() {
-        System.out.println(type + " Triangle");
+        System.out.println("Draw Triangle");
     }
 }
