fb2bf0361923c90719dfa5c6c5ddd30169240b0d
IlyaZinkovich <ilya.zinkovich@gmail.com>
2015-02-07 20:26:36 +0300

    Spring Tutorial - 09 Initializing Collections

--- src/main/java/tutorial/Point.java
+++ src/main/java/tutorial/Point.java
@@ -20,4 +20,12 @@ public class Point {
     public void setY(int y) {
         this.y = y;
     }
+
+    @Override
+    public String toString() {
+        return "Point{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
 }
